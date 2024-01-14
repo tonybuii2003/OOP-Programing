5bfc025eee64af39cec4f0ea5bb85405a3236d33
tonybuii2003 <longphibui1993@gmail.com>
2024-01-14 16:16:48 -0500

    add parent folder

--- FinalReview/src/Compress.java
+++
@@ -1,6 +0,0 @@
-public class Compress {
-    public static void main(String[] args) {
-
-    }
-
-}
--- FinalReview/src/Tartan.java
+++
@@ -1,9 +0,0 @@
-public class Tartan {
-    public static void main(String[] args) {
-
-    }
-
-    public static void tartan() {
-
-    }
-}
